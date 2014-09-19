7497539223ecc88b38556e2f32f76f771471f559
jvgriffis <jesse.griffis@nbtsolutions.com>
2014-09-19 12:04:20 -0400

    Allowing access to the underlying Express app.

--- restly.js
+++ restly.js
@@ -16,6 +16,9 @@ var restly = {};
 var express = require('express');
 var app = express();
 
+// allow access to the underlying Express app:
+restly.app = app;
+
 // force express to parse posted and putted parameters
 app.use(express.bodyParser({ keepExtensions: true, uploadDir: '/tmp' }));
 
